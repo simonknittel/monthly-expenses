b353d5702e3bc51ab5e774eb27538ff47b00f588
Simon Knittel <hallo@simonknittel.de>
2023-02-05 16:44:28 +0100

    feat: Add @vercel/analytics

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,17 +1,19 @@
 import { type AppType } from "next/app";
-
 import { api } from "../utils/api";
-
 import "../styles/globals.css";
 import { LoginProvider } from "../contexts/Login";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <LoginProvider>
-      <Component {...pageProps} />
-      <Toaster />
-    </LoginProvider>
+    <>
+      <LoginProvider>
+        <Component {...pageProps} />
+        <Toaster />
+      </LoginProvider>
+      <Analytics />
+    </>
   );
 };
 
